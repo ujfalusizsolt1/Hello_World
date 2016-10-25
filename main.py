8a972dd82b41f1d0ccb3dcfbfb538b081343a880
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2016-10-25 23:34:12 +0200

    one more function

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import sys
 def insertname(incoming):
     message = print("Hello ",incoming,"!", sep='')
     return(message)
+    
+def generalmessage():
+    message="Hello World!"
+    print(message)
 
 try:
     insertname(sys.argv[1])
 except IndexError:
-    print("Hello World!")
+    generalmessage()
